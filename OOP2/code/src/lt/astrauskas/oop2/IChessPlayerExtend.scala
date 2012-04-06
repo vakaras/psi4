c0bc63fab334ea8d69de19ee0bfc6a9f768d4926
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-04-06 21:05:56 +0300

    Pataisyta klaidelė.

--- OOP2/code/src/lt/astrauskas/oop2/IChessPlayerExtend.scala
+++ OOP2/code/src/lt/astrauskas/oop2/IChessPlayerExtend.scala
@@ -7,6 +7,5 @@ trait IChessPlayerExtend extends IChessPlayer {
   def winImpl(): Int = base.winImpl()
   def looseImpl(): Int = base.looseImpl()
   def drawImpl(): Int = base.drawImpl()
-  def playedImpl(): Int = base.playedImpl()
 
 }
