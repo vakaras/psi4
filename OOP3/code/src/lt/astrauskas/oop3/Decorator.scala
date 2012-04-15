e234c5f9a3c8fd3fc5b2fd3351b3ecd69ae8ee86
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-04-15 12:27:21 +0300

    Realizuotas programos demonstravimas.

--- OOP3/code/src/lt/astrauskas/oop3/Decorator.scala
+++ OOP3/code/src/lt/astrauskas/oop3/Decorator.scala
@@ -8,6 +8,16 @@ class Decorator(
     component = newComponent
   }
 
+  def get(manifest: Manifest[_ <: Decorator]):
+      Option[_ <: Decorator] = {
+    if (ClassManifest.singleType(this) <:< manifest)
+      Some(this)
+    else if (component.isInstanceOf[Decorator])
+      component.asInstanceOf[Decorator].get(manifest)
+    else
+      None
+    }
+
   def firstName() = component.firstName
   def lastName() = component.lastName
   override def fullName() = component.fullName
--- OOP3/code/src/lt/astrauskas/oop3/Demo.scala
+++ OOP3/code/src/lt/astrauskas/oop3/Demo.scala
@@ -2,16 +2,50 @@ package lt.astrauskas.oop3
 
 object Demo {
 
+  def show[T <: Decorator : Manifest](
+      human: Decorator, function: T => Unit) = {
+    val m = manifest[T]
+    human.get(m) match {
+      case Some(decorator)
+        if ClassManifest.singleType(decorator) <:< m
+        => function(decorator.asInstanceOf[T])
+      case _ => println("Not found: " + m.erasure.toString())
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     println("Start.")
 
     var human: IHuman = new Human("Vytautas", "Astrauskas")
     println("Full name: " + human.fullName)
 
+    var student = new Student(
+        human, "VŽG", 10)
+    println("Decorated with Student.")
+
     var withEmail = new HumanWithEmail(
-        human, "vastrauskas@gmail.com")
-    println("Full name:    " + withEmail.fullName)
-    println("Pretty email: " + withEmail.prettyEmail)
+        student, "vastrauskas@gmail.com")
+    println("Decorated with HumanWithEmail.")
+
+    show(withEmail,
+        (human: HumanWithEmail) => println(
+            "Pretty email: " + human.prettyEmail))
+    show(withEmail,
+        (human: Student) => println("School: " + human.school))
+
+    withEmail.setComponent(human)
+    println("Removed decorator Student.")
+
+    show(withEmail,
+        (human: Student) => println("School: " + human.school))
+
+    var teacher = new Teacher(
+        withEmail, "matematika", "m. m.")
+    println("Decorated with Teacher.")
+
+    show(teacher,
+        (human: Teacher) => println(
+            "Full name: " + human.fullName))
 
     println("End.")
   }
